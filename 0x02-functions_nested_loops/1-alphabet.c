bb553815193662a0c19f37d1971303c5cfe4785b
a11y-2824 <wachana.anne@gmail.com>
2022-07-06 14:10:25 -0700

    alx

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print alphabet
+ *
+ * Return 0
+ */
+int main(void)
+{
+    char l;
+    for (l='a'; l<='z'; l ++)
+    {
+            _putchar (l\n)
+    }
+    return (0);
+}
+
