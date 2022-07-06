8b942c9a1c687f97fc6292caddbf8a95de94c848
a11y-2824 <wachana.anne@gmail.com>
2022-07-06 14:04:39 -0700

    alx

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print alphabet
+ *
+ * Return 0
+ */
+int main(void)
+{
+    char l;
+    for (l='a'; l<='z'; l ++)
+    {
+	    _putchar (l\n)
+    }
+    return (0);
+}
