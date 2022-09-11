c35b0de8927d0dc084c0afea8e8e5a5f6eb00d3f
Anne Wachana <96468875+a11y-2824@users.noreply.github.com>
2022-09-11 05:05:13 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly ""Programming is like building a multilingual puzzle"
+ * main - prints exactly "Programming is like building a multilingual puzzle"
  *
  * description : prints a program using puts
  * Return : 0
  */
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle\n");
+	puts("/"Programming is like building a multilingual puzzle");
 	return (0);
 }
