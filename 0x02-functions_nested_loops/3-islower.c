295f46348fc1edfe975ed8aba599eaf6a5e792df
Anne Wachana <96468875+a11y-2824@users.noreply.github.com>
2022-09-18 22:38:25 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,9 @@
 
 /**
  * int _islower(int c) - checks for lowercase character
- * description - Returns 1 if c is lowercase
- * return - 0 or 1
+ * 
+ * Description: Returns 1 if c is lowercase
+ * Return: 0 or 1
  */
 int _islower(int c)
 {
