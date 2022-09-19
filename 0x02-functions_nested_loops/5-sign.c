4a532aaf6edc4f0a941cd4cd34742656894e86d4
Anne Wachana <96468875+a11y-2824@users.noreply.github.com>
2022-09-19 20:59:41 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,8 +2,9 @@
 
 /**
  * print_sign - prints the sign of a number
- * description -sign
- * return - is 1, 0 or -1
+ * 
+ * Description: sign
+ * Return: is 1, 0 or -1
  */
 int print_sign(int n)
 {
