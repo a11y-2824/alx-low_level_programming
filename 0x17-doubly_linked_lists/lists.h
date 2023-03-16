3fb2a7bfafd2ea542bf19b83389450ba70840722
a11y-2824 <wachana.anne@gmail.com>
2023-03-16 16:32:31 +0300

    header files

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,12 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif /* _SAMPLE_HEADER_H_ */
