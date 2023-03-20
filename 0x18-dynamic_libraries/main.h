2975213409b271f227661256cbe4eab33e1cb4f3
a11y-2824 <wachana.anne@gmail.com>
2023-03-20 15:50:08 +0300

    changed to betty style

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,15 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
+
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
 
 int _putchar(char c);
 int _islower(int c);
@@ -24,6 +31,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int main(void)
+int main(void);
 
-#endif 
+#endif
